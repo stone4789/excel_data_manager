608d247d360348f5e40fcd256b73365ca97c3b60
Dave Stonecipher <stonecipherdr@gmail.com>
2022-07-28 15:26:48 -0600

    Removed private data, used func to save and export

--- excel_data_manager.py
+++ excel_data_manager.py
@@ -42,15 +42,41 @@ def excelManager():
                     columns={
                         "Proprietary data removed here, use whatever new column names you want in a new dictionary"
                     }
+                )
                 new_df = pd.concat([new_df, temp_df], ignore_index=True, sort=False)
                 final_spreadsheet = final_spreadsheet.drop_duplicates()
-                final_spreadsheet["ID"] = final_spreadsheet["ID"].astype("str") + "-" + final_spreadsheet["Warehouse"].astype("str")
+                final_spreadsheet["ID"] = (
+                    final_spreadsheet["ID"].astype("str")
+                    + "-"
+                    + final_spreadsheet["Warehouse"].astype("str")
+                )
                 ids = final_spreadsheet["ID"].unique()
                 to_keep = []
                 for id in ids:
                     id_df = final_spreadsheet.loc[final_spreadsheet["ID"] == id]
                     if id_df["Quantity"].count() > 24:
                         to_keep.append(id)
-                final_spreadsheet = final_spreadsheet[final_spreadsheet["ID"].isin(to_keep)]
+                final_spreadsheet = final_spreadsheet[
+                    final_spreadsheet["ID"].isin(to_keep)
+                ]
         return final_spreadsheet
-                )
+
+    mks_prodhd_df = category_cleaner(mks_prodhd)
+    mks_df = category_cleaner(mks)
+    am_schd_df = category_cleaner(am_schd)
+    # Put all of them together in a single Pandas dataframe and (hopefully) export to a csv if it isn't too large
+    try:
+        df = am_schd_df.append(mks_prodhd_df)
+        df = df.append(mks_df)
+        df.to_csv("final_production_output.csv", index=False)
+    except:
+        print("DataFrame exception")
+    am_schd_df.to_csv("am_schd_finalexcelsheet.csv", index=False)
+
+    mks_df.to_csv("mks_finalexcelsheet.csv", index=False)
+
+    mks_prodhd_df.to_csv("mks_prodhd_finalexcelsheet.csv", index=False)
+
+
+if __name__ == "__main__":
+    excelManager()
