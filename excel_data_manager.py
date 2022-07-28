69487d569e3fa329122382fe02c6c387cfcc6103
Dave Stonecipher <stonecipherdr@gmail.com>
2022-07-28 14:43:51 -0600

    Added required imports

--- excel_data_manager.py
+++ excel_data_manager.py
@@ -1,1 +1,10 @@
 #! /usr/bin/env python3
+def excelManager():
+    import glob
+    import numpy as np
+    import openpyxl
+    import os
+    import pandas as pd
+    import warnings
+
+    warnings.filterwarnings("ignore")
